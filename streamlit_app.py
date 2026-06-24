4a8469b20b377f15aace67bacf5b23b8ac0f0b30
lazzarally <113340425+lazzarally@users.noreply.github.com>
2026-06-24 14:01:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,5 +70,5 @@ else:
             st.success('Your Smoothie is ordered ' + name_on_order + '!', icon="✅")
 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response)
