bedd5293277faab6043644b2d46d01810f90a697
lazzarally <113340425+lazzarally@users.noreply.github.com>
2026-06-24 15:10:36 +0800

    fix True

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ else:
 #    cnx = st.connection("snowflake")
 #    session = cnx_session()
     my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-    st.dataframe(data=my_dataframe, use_container_width=TRUE)
+    st.dataframe(data=my_dataframe, use_container_width=True)
     st.stop()
 
     ingredients_list = st.multiselect(
