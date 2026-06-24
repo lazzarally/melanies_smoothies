c65b3ac2126f7a1b5f1326142506d5167ca07511
lazzarally <113340425+lazzarally@users.noreply.github.com>
2026-06-24 14:20:07 +0800

    move code into for each fruti chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark import Session
 from snowflake.snowpark.functions import col
+import requests 
 
 # Create session
 session = Session.builder.configs(st.secrets["snowflake"]).create()
@@ -62,14 +63,13 @@ else:
         for fruit_chosen in ingredients_list:
             ingredients_string += fruit_chosen + ' '
 
+            smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+            sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
         my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order) values ('""" + ingredients_string + """', '""" + name_on_order + """') """
 
         time_to_insert = st.button('Submit Order')
         if time_to_insert:
             session.sql(my_insert_stmt).collect()
             st.success('Your Smoothie is ordered ' + name_on_order + '!', icon="✅")
-
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+ 
