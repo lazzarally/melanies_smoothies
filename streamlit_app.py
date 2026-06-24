0f032a36fa2221c4e6ab587bd85292b0d9f84f78
lazzarally <113340425+lazzarally@users.noreply.github.com>
2026-06-24 21:53:49 +0800

    fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,7 +74,7 @@ else:
             st.subheader(fruit_chosen + ' Nutrion Information')
 
             smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)  
-            sf_df = st.dataframe(data=smoothiefroot_response.json(), width=content)
+            sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
         my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order) values ('""" + ingredients_string + """', '""" + name_on_order + """') """
 
