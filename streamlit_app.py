0883388cc788cb8a46d6722edc39d1da9b8acbf0
lazzarally <113340425+lazzarally@users.noreply.github.com>
2026-06-24 15:08:01 +0800

    add new SEARCH_ON column to the df

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,9 @@ else:
 
 #    cnx = st.connection("snowflake")
 #    session = cnx_session()
-    my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+    my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+    st.dataframe(data=my_dataframe, use_container_width=TRUE)
+    st.stop()
 
     ingredients_list = st.multiselect(
         'Choose up to 5 ingredients:'
