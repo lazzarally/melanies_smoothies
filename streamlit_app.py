2520ba9211c66b3a3a57cdd3d8813a3b38399446
lazzarally <113340425+lazzarally@users.noreply.github.com>
2026-06-24 11:16:30 +0800

    Comment out Snowflake connection
    Comment out the connection to Snowflake in streamlit_app.py.

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ else:
     name_on_order = st.text_input('Name on Smoothie:')
     st.write('The name on your Smoothie will be: ', name_on_order)
 
-    cnx = st.connection("snowflake")
+#    cnx = st.connection("snowflake")
     session = cnx_session()
     my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
